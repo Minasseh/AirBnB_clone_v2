d85203757687a1c121a1753f111b72e372267853
Minasseh <mnsalaro@yahoo.com>
2024-02-01 22:54:09 +0300

    changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,3 +21,26 @@ echo "<html>
 
 # Create symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By 2140-web-01;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /redirect_me {
+        return 301 http://minasseh.me/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;}
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Fabric file that generates .tgz file"""
+
+from fabric.api import local, env
+from datetime import datetime
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder."""
+
+    time_stamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_name = f"versions/web_static_{time_stamp}.tgz"
+
+    try:
+        # Create the 'versions' directory if it doesn't exist
+        local("mkdir -p versions")
+
+        # Generate the .tgz archive using a timestamped filename
+        local(f"tar -cvzf {file_name} web_static")
+
+        return file_name  # Return the filename for further use if needed
+
+    except Exception as e:
+        print(f"Error creating archive: {e}")
+        return None
+
+
